bf542ce6285d707e29889723472b16e7d3655250
Or Hasson <hassonor@gmail.com>
2022-01-12 15:30:35 +0200

    change to rglob -> same as glob(**/*.txt)

--- files-and-folder-operations/change-file-extensions.py
+++ files-and-folder-operations/change-file-extensions.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 root_dir = Path('files4')
 
-for path in root_dir.glob("*"):
+for path in root_dir.rglob("*.txt"):
     if path.is_file():
         new_filepath = path.with_suffix(".csv")
         path.rename(new_filepath)
