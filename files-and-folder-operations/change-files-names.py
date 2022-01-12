92269d36b608527ff20050c8de86f8822ac204d2
Or Hasson <hassonor@gmail.com>
2022-01-12 15:06:33 +0200

    example adding year and month to file in sub sub folder

---
+++ files-and-folder-operations/change-files-names.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+root_dir = Path('files2')
+file_paths = root_dir.glob("**/*")
+
+for path in file_paths:
+    if path.is_file():
+        parent_parent = path.parts
+        sub_folders = path.parts[1:-1]
+        new_filename = "-".join(sub_folders) + '-' + path.name
+        new_filepath = path.with_name(new_filename)  # set the current dir to the correct dir
+        path.rename(new_filepath)  # change the filename to new filename in the correct dir
