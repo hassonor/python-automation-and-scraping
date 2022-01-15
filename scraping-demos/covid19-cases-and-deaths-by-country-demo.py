47c600b4f1b40abf75dcaf7e5cbb3cf6c68fff7b
Or Hasson <hassonor@gmail.com>
2022-01-15 23:56:33 +0200

    add headless option

--- scraping-demos/covid19-cases-and-deaths-by-country-demo.py
+++ scraping-demos/covid19-cases-and-deaths-by-country-demo.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 
 def get_driver(url):
     options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
     options.add_argument('disable-infobars')
     options.add_argument('start-maximized')
     options.add_argument('disable-dev-shm-usage')
