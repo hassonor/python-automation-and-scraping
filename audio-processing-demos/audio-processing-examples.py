398b9c3879ca875f205557918b91e5d3ec2a3672
Or Hasson <hassonor@gmail.com>
2022-01-16 13:52:39 +0200

    adding audio-processing example

--- audio-processing-demos/audio-processing-examples.py
+++ audio-processing-demos/audio-processing-examples.py
@@ -1,3 +1,30 @@
+# download ffmpeg and unzip to "C:\ffmpeg" and set the path var "C:\ffmpeg\bin"
+# you can dowonload it form here https://www.ffmpeg.org/
+# see also this:  https://stackoverflow.com/questions/51219531/pydub-unable-to-locate-ffprobe
+
+# Enjoy your mix DJ :-)
+
+
 from pydub import AudioSegment
 
-original = AudioSegment.from_mp3('Gareth_Emery_feat_Lucy_Saunders _Sanctuary.mp3')
+AudioSegment.converter = "C:\\ffmpeg\\bin\\ffmpeg.exe"
+AudioSegment.ffmpeg = "C:\\ffmpeg\\bin\\ffplay.exe"
+AudioSegment.ffprobe = "C:\\ffmpeg\\bin\\ffprobe.exe"
+print(AudioSegment.converter)
+
+
+def create_reverse_and_merged_audio_file(file_path):
+    original = AudioSegment.from_wav(file_path)
+    print(type(original))
+    print(original)
+    reversed_audio = original.reverse()  # reversed the original audio
+    reversed_audio.export('reversed.wav')
+    reversed_audio = reversed_audio + 15  # increase volume by 15
+    first_two = original[0:5000]  # 5 seconds long
+    first_two.export('first_5_seconds.wav')
+    merged = original + AudioSegment.silent(
+        1000) + reversed_audio  # add the original file the reversed audio + Adding 1s silence
+    merged.export('merged_original_reversed.wav')
+
+
+create_reverse_and_merged_audio_file('sample.wav')
